fa0b0fd13d51fc8041bebd9c2cd1d21f8bb57c3e
amalik13 <GBTC440014ur@GBTC363I14W7.wdce.local>
2015-10-08 11:42:10 -0400

    commit

--- Magic8ball/src/magicPackage/MagicApp.java
+++ Magic8ball/src/magicPackage/MagicApp.java
@@ -2,21 +2,43 @@ package magicPackage;
 
 import java.util.Scanner;
 
-public class MagicApp {
+public class MagicApp extends magic8Class{
 
 	public static void main(String[] args) {
 		String message = "";
 		String Question = "";
-		magic8Class obj1 = new magic8Class();
-		
-		Scanner Keyboard = new Scanner(System.in);
+		String option = "1";
+		MagicApp obj1 = new MagicApp();
+		Magic8History history = new Magic8History();
 		System.out.println("...............Welcome to the 8 Ball game........... ");
+		
+		
+		while("1".equalsIgnoreCase(option))
+			
+		{
+		Scanner Keyboard = new Scanner(System.in);
+		
 		System.out.println(" Write the question and I shall have the answer for you");
 		
-		Question = Keyboard.next();
+		Question = Keyboard.nextLine();
+		history.SaveQuestion(Question);
 		System.out.println("");
-		 message = obj1.shakeit();
-		 System.out.println( "MAGIC 8-BALL SAYS: " + message );
+		message = obj1.shakeit();
+		history.SaveAnswers(message);
+		System.out.println( "MAGIC 8-BALL SAYS: " + message );
+		
+		System.out.println("press button for the option 1) Play again 2) Show History 3) exit");
+		 option = Keyboard.nextLine();
+		while (option.equalsIgnoreCase("2"))
+		{
+			history.printHistory();
+			System.out.println("press button for the option 1) Play again 2) Show History 3) exit");
+			option = Keyboard.nextLine();
+			
+		}
+		//Keyboard.close();
+		}
+		
 		
 	}
 }
