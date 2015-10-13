3dbdc6bc67607561cc050d35c71c01a3a4ba580e
amalik13 <GBTC440014UR@GBTC363I14W7.wdce.local>
2015-10-13 11:19:43 -0400

    commiting

---
+++ HashMap/src/HashMapPack/FirstMap.java
@@ -0,0 +1,36 @@
+package HashMapPack;
+
+import java.util.*;
+import java.util.Scanner;
+
+public class FirstMap {
+	public static void main(String[] args) {
+		
+		HashMap<Integer, String> myMap = new HashMap<Integer, String>();
+		
+		int number=0;
+		myMap.put(1,"The value is one");
+		myMap.put(2,"The value is two");
+		myMap.put(3,"The value is three");
+		myMap.put(4,"The value is four");
+		myMap.put(5,"The value is five");
+		myMap.put(6,"The value is six");
+		myMap.put(7,"The value is seven");
+		myMap.put(8,"The value is eight");
+		myMap.put(9,"The value is nine");
+		myMap.put(10,"The value is ten");
+		System.out.println("enter the number");
+		Scanner scan = new Scanner(System.in);
+		 number = scan.nextInt();
+		while(number>=1 && number<=10)
+		{
+		
+		 System.out.println(myMap.get(number));
+		 System.out.println("enter the number");
+		 number = scan.nextInt();
+		}
+		
+		
+	}
+
+}
